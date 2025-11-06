1cfd939045790be5a32ebcf637c28f113021c83b
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-11-06 15:13:22 +0000

    feat: entrega da aula 3 e ficheiro info.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # A3_Entrega
+O meu primeiro commit do módulo, pretendo aprender da forma mais eficiente possível e, para isso me dedicar ao máximo. Daniel Moraes
---
+++ info.js
@@ -0,0 +1,4 @@
+console.log("Comidas preferidas: Churrasco, pastel, feijoada, arroz com pequi e muitas e muitas outras");
+console.log("Bedidas preferidas: Coca-cola, sumos naturais, cerveja e vinho");
+console.log("Cores preferidas: preto e azul");
+console.log("Hobbies: jogar videogame, cozinhar, colecionar e testar tecnologias");
